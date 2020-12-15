7613781890aca91255ed3db2d0573c9c6a45f5d7
lmedina <lmedinapalacios078@gmail.com>
2020-12-15 15:40:07 -0600

    Commit3

---
+++ windowsForm1/FormPrincipal.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace windowsForm1
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxNom = new System.Windows.Forms.TextBox();
+            this.textApelli = new System.Windows.Forms.TextBox();
+            this.buttnAgregar = new System.Windows.Forms.Button();
+            this.dgvCliente = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textSala = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttModificar = new System.Windows.Forms.Button();
+            this.buttElimi = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttCargar = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textID = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCliente)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.Location = new System.Drawing.Point(481, 463);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(40, 19);
+            this.buttonSalir.TabIndex = 2;
+            this.buttonSalir.Text = "Salir ";
+            this.buttonSalir.UseVisualStyleBackColor = true;
+            this.buttonSalir.Click += new System.EventHandler(this.buttonSalir_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nombre:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Apellido:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBoxNom
+            // 
+            this.textBoxNom.Location = new System.Drawing.Point(67, 67);
+            this.textBoxNom.Name = "textBoxNom";
+            this.textBoxNom.Size = new System.Drawing.Size(185, 20);
+            this.textBoxNom.TabIndex = 5;
+            this.textBoxNom.TextChanged += new System.EventHandler(this.textBoxNom_TextChanged);
+            // 
+            // textApelli
+            // 
+            this.textApelli.Location = new System.Drawing.Point(67, 96);
+            this.textApelli.Name = "textApelli";
+            this.textApelli.Size = new System.Drawing.Size(185, 20);
+            this.textApelli.TabIndex = 6;
+            this.textApelli.TextChanged += new System.EventHandler(this.textBoxCiud_TextChanged);
+            // 
+            // buttnAgregar
+            // 
+            this.buttnAgregar.Location = new System.Drawing.Point(6, 31);
+            this.buttnAgregar.Name = "buttnAgregar";
+            this.buttnAgregar.Size = new System.Drawing.Size(106, 52);
+            this.buttnAgregar.TabIndex = 7;
+            this.buttnAgregar.Text = "Agregar";
+            this.buttnAgregar.UseVisualStyleBackColor = true;
+            this.buttnAgregar.Click += new System.EventHandler(this.buttnEnviar_Click);
+            // 
+            // dgvCliente
+            // 
+            this.dgvCliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCliente.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgvCliente.Location = new System.Drawing.Point(15, 33);
+            this.dgvCliente.Name = "dgvCliente";
+            this.dgvCliente.Size = new System.Drawing.Size(342, 190);
+            this.dgvCliente.TabIndex = 8;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nombre ";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Apellido";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Salario";
+            this.Column3.Name = "Column3";
+            // 
+            // textSala
+            // 
+            this.textSala.Location = new System.Drawing.Point(67, 122);
+            this.textSala.Name = "textSala";
+            this.textSala.Size = new System.Drawing.Size(185, 20);
+            this.textSala.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Salario:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 9;
+            // 
+            // buttModificar
+            // 
+            this.buttModificar.Location = new System.Drawing.Point(6, 89);
+            this.buttModificar.Name = "buttModificar";
+            this.buttModificar.Size = new System.Drawing.Size(106, 52);
+            this.buttModificar.TabIndex = 12;
+            this.buttModificar.Text = "Modificar";
+            this.buttModificar.UseVisualStyleBackColor = true;
+            this.buttModificar.Click += new System.EventHandler(this.buttModificar_Click);
+            // 
+            // buttElimi
+            // 
+            this.buttElimi.Location = new System.Drawing.Point(6, 147);
+            this.buttElimi.Name = "buttElimi";
+            this.buttElimi.Size = new System.Drawing.Size(106, 52);
+            this.buttElimi.TabIndex = 13;
+            this.buttElimi.Text = "Eliminar";
+            this.buttElimi.UseVisualStyleBackColor = true;
+            this.buttElimi.Click += new System.EventHandler(this.buttElimi_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(348, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(299, 15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Buscar:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.textID);
+            this.groupBox1.Controls.Add(this.textApelli);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textSala);
+            this.groupBox1.Controls.Add(this.textBoxNom);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(284, 173);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Agregar Cliente";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttCargar);
+            this.groupBox2.Controls.Add(this.buttModificar);
+            this.groupBox2.Controls.Add(this.buttnAgregar);
+            this.groupBox2.Controls.Add(this.buttElimi);
+            this.groupBox2.Location = new System.Drawing.Point(12, 191);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(118, 281);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Acciones";
+            // 
+            // buttCargar
+            // 
+            this.buttCargar.Location = new System.Drawing.Point(6, 205);
+            this.buttCargar.Name = "buttCargar";
+            this.buttCargar.Size = new System.Drawing.Size(106, 52);
+            this.buttCargar.TabIndex = 19;
+            this.buttCargar.Text = "Cargar";
+            this.buttCargar.UseVisualStyleBackColor = true;
+            this.buttCargar.Click += new System.EventHandler(this.buttCargar_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dgvCliente);
+            this.groupBox3.Location = new System.Drawing.Point(146, 222);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(363, 229);
+            this.groupBox3.TabIndex = 18;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Clientes";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(35, 48);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "ID:";
+            // 
+            // textID
+            // 
+            this.textID.Location = new System.Drawing.Point(67, 41);
+            this.textID.Multiline = true;
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(37, 20);
+            this.textID.TabIndex = 13;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 482);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.buttonSalir);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Name = "FormPrincipal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCliente)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSalir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxNom;
+        private System.Windows.Forms.TextBox textApelli;
+        private System.Windows.Forms.Button buttnAgregar;
+        private System.Windows.Forms.DataGridView dgvCliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.TextBox textSala;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttModificar;
+        private System.Windows.Forms.Button buttElimi;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button buttCargar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textID;
+    }
+}
+
---
+++ windowsForm1/FormPrincipal.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windowsForm1
+{
+   
+    public partial class FormPrincipal : Form
+    {
+        class Cliente
+        {
+            public int Id;
+            public string Nombre;
+            public string Apellido;
+            public float Salario;
+
+        }
+
+        List<Cliente> lsClientes = new List<Cliente>();
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dios Primero Pasare Programacion 2", " ", MessageBoxButtons.YesNoCancel);
+           
+        }
+       
+        private void cargarClie()
+        {
+            dgvCliente.Rows.Clear();
+                
+            foreach(Cliente c in lsClientes)
+            {
+                dgvCliente.Rows.Add(c.Id, c.Nombre, c.Apellido, c.Salario);
+            }
+        }
+         private void Datos()
+        {
+
+            Cliente c = new Cliente();
+            c.Id =int.Parse(textID.Text.Trim());
+            c.Nombre = textBoxNom.Text.Trim();
+            c.Apellido = textApelli.Text.Trim();
+            c.Salario = float.Parse(textSala.Text.Trim());
+            lsClientes.Add(c);
+
+           
+            
+        }
+        private void limpiarCampos()
+        {
+            textID.Text = string.Empty; 
+            textBoxNom.Text = string.Empty;
+            textApelli.Text = string.Empty;
+            textSala.Text = string.Empty;
+
+        }
+        private void AgregarClientes()
+        {
+            try
+            {
+                Datos();
+                MessageBox.Show("Registro Agregado");
+                limpiarCampos();
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Error" + ex.Message);
+            }
+        }
+
+        private void Cambiar(bool e)
+        {
+            dgvCliente.Enabled= e;
+            buttnAgregar.Enabled = e;
+            buttCargar.Enabled = e;
+            buttElimi.Enabled = e;
+            textBoxNom.Enabled = e;
+            textApelli.Enabled = e;
+
+        }
+    
+        public void Salir()
+        {
+
+            if (MessageBox.Show("Quieres salir de la app", "Mi aplicacion ", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+        }
+
+        private void buttonSalir_Click(object sender, EventArgs e)
+        {
+            Salir();
+
+        }
+
+        private void buttnEnviar_Click(object sender, EventArgs e)
+        {
+            AgregarClientes();
+        }
+
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxNom_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxCiud_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttCargar_Click(object sender, EventArgs e)
+        {
+            cargarClie();
+        }
+
+        private void buttModificar_Click(object sender, EventArgs e)
+        {
+            if (dgvCliente.Rows.Count > 0)
+            {
+                 DataGridViewRow FilasSelec = dgvCliente.CurrentRow;
+                Cambiar(false);
+
+                if(buttModificar.Text=="Modificar Cliente")
+                {
+                    dgvCliente.Enabled = false;
+                    buttModificar.Text = "Guardar Cambios";
+
+                    textID.Text = FilasSelec.Cells[0].Value.ToString();
+                    textBoxNom.Text = FilasSelec.Cells[1].Value.ToString();
+                    textApelli.Text = FilasSelec.Cells[2].Value.ToString();
+                    textSala.Text = FilasSelec.Cells[3].Value.ToString();
+                }
+                else
+                {
+                    try
+                    {
+                        buttModificar.Text = "Modificar Cliente";
+
+                        string sal = float.Parse(textSala.Text.Trim()).ToString("C2");
+                        dgvCliente.Rows[FilasSelec.Index].Cells[3].Value = sal;
+
+                        lsClientes.FirstOrDefault(c => c.Id == Convert.ToInt32(textID.Text.Trim())).Salario = float.Parse(textSala.Text.Trim());
+
+                        MessageBox.Show("Se ha modificado el registro ");
+                        limpiarCampos();
+                        Cambiar(true);
+                        
+                    }catch(Exception ex)
+                    {
+                        MessageBox.Show("Error" + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void buttElimi_Click(object sender, EventArgs e)
+        {
+
+            if(dgvCliente.Rows.Count> 0)
+            {
+                if(MessageBox.Show("Esta seguro de eliminar el registro", "Borrado", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+                {
+                    DataGridViewRow FilaSelect = dgvCliente
+                }
+
+            }
+
+        }
+    }
+}
